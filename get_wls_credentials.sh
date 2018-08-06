be97acb84f46b08a93a1473687ad7dc1acaf709b
Youssef Sbai Idrissi <sbaiidrissiyoussef@gmail.com>
2018-08-06 17:08:06 +0100

    Add files via upload
    Useful script to get Weblogic Credentials.

---
+++ get_wls_credentials.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+ 
+touch /tmp/decrypt_password.py 
+ 
+echo ""; echo ""; echo " DECRYPTING WEBLOGIC CREDENTIALS " echo " Note : Run This script as Root "; echo ""; echo "";   echo " *************************************************************** "; echo ""; 
+ 
+find / -name "setDomainEnv.sh"  
+ 
+echo "" echo " *************************************************************** "; echo ""; 
+ 
+read -p " Enter the appropriate setDomainEnv.sh file : " domainenv_var 
+ 
+source $domainenv_var 
+ 
+echo " *************************************************************** "; echo""; 
+ 
+find / -name "boot.properties" 
+ 
+echo "" echo " *************************************************************** "; echo ""; 
+ 
+read -p " Enter the appropriate boot.properties file : " bootprop_var ; echo""; 
+  
+boot_prp_path=`find $DOMAIN_HOME/servers/ -name "boot.properties" -print`
+
+wls_usr=`grep username $boot_prp_path | sed -e "s/^username=\(.*\)/\1/"` wls_pwd=`grep password $boot_prp_path | sed -e "s/^password=\(.*\)/\1/"` 
+ 
+echo " Encrypted Weblogic Username : $wls_usr " echo " Encrypted Weblogic Password : $wls_pwd "; echo ""; 
+ 
+echo " *************************************************************** "; echo ""; 
+ 
+cat <<EOT >/tmp/decrypt_password.py from weblogic.security.internal import * from weblogic.security.internal.encryption import * encryptionService = SerializedSystemIni.getEncryptionService(".") clearOrEncryptService = ClearOrEncryptedService(encryptionService) # Take encrypt password from user #usr = raw_input(" Paste the encrypted Username ({AES}abcd1234) : ") #pwd = raw_input(" Paste the encrypted Password ({AES}abcd1234) : ") usr = str('$wls_usr') pwd = str('$wls_pwd') # Delete unnecessary escape characters #preppwd = pwd.replace("\\","") # Display password print "\n\n\n " print " ************************************** \n\n" print "  Username   :   " + clearOrEncryptService.decrypt(usr) print "  Password   :   " + clearOrEncryptService.decrypt(pwd) print " \n\n" print " ************************************** \n\n" EOT 
+ 
+cd $DOMAIN_HOME/security/ 
+ 
+echo ""; echo ""; java weblogic.WLST /tmp/decrypt_password.py $wls_usr 
+ 
+rm -rf /tmp/decrypt_password.py;  
