46e3fd43808d1cbc37b26e833214a399d32d0ed5
Youssef Sbai Idrissi <sbaiidrissiyoussef@gmail.com>
2023-09-19 22:35:19 +0100

    Update convert_jsonl_to_json.py

--- convert_jsonl_to_json.py
+++ convert_jsonl_to_json.py
@@ -1,29 +1,50 @@
-BUFFER_SIZE = 50 * 1024 * 1024  # 10 MB
-
-with open('isbndb_2022_09.jsonl', 'r') as infile, open('isbndb_2022_09.json', 'w') as outfile:
-    outfile.write('[')  # Start JSON array
-    buffer = ""
-    first = True  # Flag to handle commas between JSON objects
+def convert_jsonl_to_json(jsonl_file_path, json_file_path, buffer_size):
+    """Convert a JSONL file to a standard JSON file.
     
-    while True:
-        chunk = infile.read(BUFFER_SIZE)
-        if not chunk:
-            break  # End of file
-        buffer += chunk
-        lines = buffer.split('\n')
+    Parameters:
+        jsonl_file_path (str): The path to the source JSONL file.
+        json_file_path (str): The path where the converted JSON file will be saved.
+        buffer_size (int): The size of the read buffer in bytes.
+    """
+    with open(jsonl_file_path, 'r') as infile, open(json_file_path, 'w') as outfile:
+        # Write the opening bracket for the JSON array
+        outfile.write('[')
         
-        for i in range(len(lines) - 1):
-            if first:
-                first = False
-            else:
-                outfile.write(',')
-            outfile.write(lines[i])
-        
-        buffer = lines[-1]  # Keep the last line in the buffer, as it might be incomplete
+        # Initialize buffer and a flag to manage object separators
+        buffer = ""
+        is_first_object = True
 
-    if buffer:  # Write any remaining content
-        if not first:
-            outfile.write(',')
-        outfile.write(buffer)
-        
-    outfile.write(']')  # End JSON array
+        while True:
+            chunk = infile.read(buffer_size)
+            if not chunk:
+                break  # End of file reached
+            
+            buffer += chunk
+            lines = buffer.split('\n')
+            
+            for i in range(len(lines) - 1):
+                # Add a comma separator for all objects after the first
+                if not is_first_object:
+                    outfile.write(',')
+                outfile.write(lines[i])
+                
+                is_first_object = False  # Update flag after writing the first object
+            
+            # Keep the last (potentially incomplete) line for the next iteration
+            buffer = lines[-1]
+
+        # Write any remaining content in buffer
+        if buffer:
+            if not is_first_object:
+                outfile.write(',')
+            outfile.write(buffer)
+
+        # Write the closing bracket for the JSON array
+        outfile.write(']')
+
+
+# Constants
+BUFFER_SIZE = 50 * 1024 * 1024  # 50 MB
+
+# Convert JSONL to JSON
+convert_jsonl_to_json('isbndb_2022_09.jsonl', 'isbndb_2022_09.json', BUFFER_SIZE)
