48d95a62095ce2031f5558ec6a9884374d5fa44a
Youssef Sbai Idrissi <sbaiidrissiyoussef@gmail.com>
2023-09-20 02:30:03 +0100

    Update simulate_decompression.py

--- simulate_decompression.py
+++ simulate_decompression.py
@@ -1,5 +1,12 @@
-import gzip
+
 import sys
+import gzip
+import bz2
+import lzma
+import zipfile
+import tarfile
+import os
+from pyunpack import Archive
 
 def convert_bytes(num):
     for unit in ['bytes', 'KB', 'MB', 'GB', 'TB']:
@@ -7,21 +14,58 @@ def convert_bytes(num):
             return f"{num:3.1f} {unit}"
         num /= 1024.0
 
-def simulate_gzip_decompression(gzip_file_path):
+def get_file_format(file_path):
+    return os.path.splitext(file_path)[1].lower()
+
+def simulate_zip_decompression(zip_file_path):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        return sum(info.file_size for info in zip_ref.infolist())
+
+def simulate_tar_decompression(tar_file_path):
+    with tarfile.open(tar_file_path, 'r') as tar_ref:
+        return sum(info.size for info in tar_ref)
+
+def simulate_other_decompression(file_path):
+    # This is a placeholder. Actual decompression simulation for other formats can be complex and may require third-party tools.
+    return None
+
+def simulate_decompression(file_path):
     total_size = 0
-    with gzip.open(gzip_file_path, 'rb') as f:
-        while True:
-            chunk = f.read(1024)  # Read only 1KB at a time
-            if not chunk:  # Break loop when EOF is reached
-                break
-            total_size += len(chunk)
-    return total_size
+    file_format = get_file_format(file_path)
+    
+    decompressors = {
+        '.gz': gzip.open,
+        '.bz2': bz2.open,
+        '.xz': lzma.open,
+        '.zip': simulate_zip_decompression,
+        '.tar': simulate_tar_decompression,
+        '.7z': simulate_other_decompression,
+        '.ear': simulate_other_decompression,
+        '.rar': simulate_other_decompression
+    }
+    
+    decompressor = decompressors.get(file_format)
+    
+    if decompressor in [gzip.open, bz2.open, lzma.open]:
+        with decompressor(file_path, 'rb') as f:
+            while True:
+                chunk = f.read(1024)
+                if not chunk:
+                    break
+                total_size += len(chunk)
+        return total_size
+    elif decompressor:
+        return decompressor(file_path)
+    else:
+        print("Unsupported or unknown file format.")
+        return None
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        gzip_file_path = sys.argv[1]
-        uncompressed_size = simulate_gzip_decompression(gzip_file_path)
-        human_readable_size = convert_bytes(uncompressed_size)
-        print(f"Simulated uncompressed size: {human_readable_size}")
+        file_path = sys.argv[1]
+        uncompressed_size = simulate_decompression(file_path)
+        if uncompressed_size is not None:
+            human_readable_size = convert_bytes(uncompressed_size)
+            print(f"Simulated uncompressed size: {human_readable_size}")
     else:
-        print("Please provide the path to the gzip file as an argument.")
+        print("Usage: python script.py <File_Path>")
