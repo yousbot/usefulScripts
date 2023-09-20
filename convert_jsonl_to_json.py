77f644b46380d2064cce7ae61885f3696b1c34c2
Youssef Sbai Idrissi <sbaiidrissiyoussef@gmail.com>
2023-09-20 02:19:42 +0100

    Update convert_jsonl_to_json.py

--- convert_jsonl_to_json.py
+++ convert_jsonl_to_json.py
@@ -1,3 +1,6 @@
+
+import sys
+
 def convert_jsonl_to_json(jsonl_file_path, json_file_path, buffer_size):
     """Convert a JSONL file to a standard JSON file.
     
@@ -7,44 +10,38 @@ def convert_jsonl_to_json(jsonl_file_path, json_file_path, buffer_size):
         buffer_size (int): The size of the read buffer in bytes.
     """
     with open(jsonl_file_path, 'r') as infile, open(json_file_path, 'w') as outfile:
-        # Write the opening bracket for the JSON array
         outfile.write('[')
-        
-        # Initialize buffer and a flag to manage object separators
         buffer = ""
         is_first_object = True
 
         while True:
             chunk = infile.read(buffer_size)
             if not chunk:
-                break  # End of file reached
+                break
             
             buffer += chunk
             lines = buffer.split('\n')
             
             for i in range(len(lines) - 1):
-                # Add a comma separator for all objects after the first
                 if not is_first_object:
                     outfile.write(',')
                 outfile.write(lines[i])
-                
-                is_first_object = False  # Update flag after writing the first object
+                is_first_object = False
             
-            # Keep the last (potentially incomplete) line for the next iteration
             buffer = lines[-1]
 
-        # Write any remaining content in buffer
         if buffer:
             if not is_first_object:
                 outfile.write(',')
             outfile.write(buffer)
 
-        # Write the closing bracket for the JSON array
         outfile.write(']')
 
-
-# Constants
-BUFFER_SIZE = 50 * 1024 * 1024  # 50 MB
-
-# Convert JSONL to JSON
-convert_jsonl_to_json('isbndb_2022_09.jsonl', 'isbndb_2022_09.json', BUFFER_SIZE)
+if __name__ == "__main__":
+    if len(sys.argv) > 3:
+        jsonl_file_path = sys.argv[1]
+        json_file_path = sys.argv[2]
+        buffer_size = int(sys.argv[3])
+        convert_jsonl_to_json(jsonl_file_path, json_file_path, buffer_size)
+    else:
+        print("Usage: python script.py <JSONL_File_Path> <JSON_File_Path> <Buffer_Size>")
